77abc64f4fb9f7a71008fb027383bc3a403fcea1
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-10-17 19:34:45 -0400

    Update bruno-toolbox.js

--- assets/js/bruno-toolbox.js
+++ assets/js/bruno-toolbox.js
@@ -3,3 +3,27 @@ const CSVToArray = (data, delimeter = ",", omitFirstRow = false) =>
     .slice(omitFirstRow ? data.indexOf("\n") + 1 : 0)
     .split("\n")
     .map(v => v.split(delimeter));
+const UUIDGenerator = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+const ArrayToCSV = (arr, delimiter = ',') =>
+  arr
+    .map(v =>
+      v.map(x => (isNaN(x) ? `"${x.replace(/"/g, '""')}"` : x)).join(delimiter)
+    )
+    .join('\n');
+const attempt = (func, ...args) => {
+  try {
+    return func(...args);
+  } catch (e) {
+    return e instanceof Error ? e : new Error(e);
+  }
+};
+const GCD = (...nums) => {
+  const _gcd = (x, y) => (!y ? x : GCD(y, x % y));
+  return [...nums].reduce((a, b) => _gcd(a, b));
+};
